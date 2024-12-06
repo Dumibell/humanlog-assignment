f7b2dcefa2c53f1a41eb9d27fcc499d3ec1853de
yejee-13hz <choyejee@13hz.kr>
2024-12-06 11:44:41 +0900

    Feat: Add example usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CustomAsciinemaPlayer = dynamic(
+  () => import("@/components/CustomAsciinemaPlayer"),
+  {
+    ssr: false,
+  }
+);
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-10 justify-center items-center h-screen">
+      <p className="text-2xl font-semibold">Asciinema Player Example</p>
+      <CustomAsciinemaPlayer src="/cast/input.cast" />
+    </div>
+  );
 }
