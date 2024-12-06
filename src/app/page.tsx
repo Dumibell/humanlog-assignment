943397d7932756dc766e2cfa43492015a72355f5
dumibell <choyejee14@gmail.com>
2024-12-06 13:36:30 +0900

    Fix: prevent layout shift when player loads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,11 @@ const CustomAsciinemaPlayer = dynamic(
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10 justify-center items-center h-screen">
-      <p className="text-2xl font-semibold">Asciinema Player Example</p>
-      <CustomAsciinemaPlayer src="/cast/input.cast" />
+    <div className="flex items-center h-screen">
+      <div className="w-screen flex flex-col items-center min-h-[400px] gap-10">
+        <p className="text-2xl font-semibold">Asciinema Player Example</p>
+        <CustomAsciinemaPlayer src="/cast/input.cast" />
+      </div>
     </div>
   );
 }
